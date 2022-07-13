992344f1715de3c868eb610b72ac5e1ad188bb8d
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-13 11:42:33 -0500

    post working in insomnia

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 // instantiate the server
 const app = express();
 
-// route for front end to request data from
+// parsing incoming data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+function createNewNote(body, notesArray){
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './data/notes.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
+
+    return note;
+}
+
+// get route for front end to request data 
 const { notes } = require('./data/notes');
 app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+// post method 
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+
     res.json(notes);
 });
 
