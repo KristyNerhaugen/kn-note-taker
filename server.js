dbd583bae9e50a43b1f985b76d5fbf41766f5ad8
kristynerhaugen <kristynerhaugen@gmail.com>
2022-07-13 16:49:01 -0500

    routes to index and notes html pages working

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
 
+// middleware added using express.static 
+app.use(express.static('public'));
+
 // parsing incoming data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +24,16 @@ function createNewNote(body, notesArray){
     return note;
 }
 
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+      return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+      return false;
+    }
+    return true;
+  }
+
 // get route for front end to request data 
 const { notes } = require('./data/notes');
 app.get('/api/notes', (req, res) => {
@@ -30,11 +43,29 @@ app.get('/api/notes', (req, res) => {
 // post method 
 app.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
-
+    // if any data in req.body is incorrecnt, send 400 error back
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
     const note = createNewNote(req.body, notes);
-
     res.json(notes);
+    }
 });
+
+// route for notes.html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// route to connect to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// wildcard route to return to main index.html page if user tries to navigate to page that doesn't exist
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 // method to make server listen 
 app.listen(3001, () => {
